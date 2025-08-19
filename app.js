b43d60429e01ece5669664f0786b166595e937cc
Alex Becci <beccialex2002@gmail.com>
2025-08-18 23:26:09 -0300

    introduction

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const msg = 'hola mundo';
+const hero = {
+    name: "IronMan",
+    age: 45
+};
+hero.age = 50;
+console.log(hero.age + 1);
+console.log(msg);
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,9 @@
+const msg: string = 'hola mundo';
+const hero ={
+    name:"IronMan",
+    age:45
+}
+
+hero.age=50
+console.log(hero.age + 1)
+console.log(msg)
