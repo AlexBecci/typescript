2a879e3d313beef588e5e463bae517e7ddf51563
alex <beccialex2002@gmail.com>
2025-09-10 18:58:59 -0300

    copy to cloud code nest

---
+++ PokedexTest/src/seed/interfaces/poke-response.interface.ts
@@ -0,0 +1,12 @@
+//generado por resopnse total
+export interface PokeResponse {
+    count: number;
+    next: string;
+    previous: null;
+    results: Result[];
+}
+
+export interface Result {
+    name: string;
+    url: string
+}
